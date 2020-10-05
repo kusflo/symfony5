fd9aa3f598b6e5bdf4894fb1fc778c333ef71267
Marcos Redondo <kusflo@gmail.com>
2020-10-05 19:00:23 +0200

    Configuring dashboard Easy Admin

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -43,6 +43,11 @@ class Comment
      */
     private $conference;
 
+    public function __toString()
+    {
+        return (string) $this->getEmail();
+    }
+
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
