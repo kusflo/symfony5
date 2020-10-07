bce6d56e6c3bd03381d7cbb14d87c914a4b18eec
Marcos Redondo <kusflo@gmail.com>
2020-10-07 12:01:48 +0200

    Configuring Comments Paginator

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -23,6 +23,19 @@ class CommentRepository extends ServiceEntityRepository
         parent::__construct($registry, Comment::class);
     }
 
+    public function getCommentPaginator(Conference $conference, int $offset)
+    {
+        $query = $this->createQueryBuilder('c')
+            ->andWhere('c.conference = :conference')
+            ->setParameter('conference', $conference)
+            ->orderBy('c.createdAt', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery();
+        return new Paginator($query);
+
+    }
+
     // /**
     //  * @return Comment[] Returns an array of Comment objects
     //  */
